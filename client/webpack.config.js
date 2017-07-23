76c198ab9ca565c00d17dc6ee0840f9a202630a8
Orferus <david.nvalderrama@gmail.com>
2017-07-23 09:59:19 -0500

    update repo before working

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -21,7 +21,7 @@ const config = {
       'es5-shim/es5-shim',
       'es5-shim/es5-sham',
       'babel-polyfill',
-      './app/bundles/LandingPage/startup/registration',
+      './app/bundles/CookpadApp/startup/registration',
     ],
   },
 
